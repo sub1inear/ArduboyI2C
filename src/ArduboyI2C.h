fd0e58543998d9b76a7e99081b06ffcb7e08bdaa
unknown <odoul28@gmail.com>
2026-03-09 20:59:19 -0700

    Remove extra period

--- src/ArduboyI2C.h
+++ src/ArduboyI2C.h
@@ -909,7 +909,7 @@ default:
     );
 }
 
-// C++ ISR version for reference.
+// C++ ISR version for reference
 #if 0
 ISR(TWI_vect) {
     switch (TWSR) { // prescaler bits are cleared, no mask needed
