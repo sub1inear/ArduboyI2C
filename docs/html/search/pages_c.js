5e11409702024286a05b3f161fc877b914ff430a
unknown <odoul28@gmail.com>
2026-06-25 11:05:15 -0700

    Add missing doc files

---
+++ docs/html/search/pages_c.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['why_20arduboyi2c_20over_20wire_0',['Why ArduboyI2C over Wire?',['../index.html#autotoc_md3',1,'']]],
+  ['wire_1',['Why ArduboyI2C over Wire?',['../index.html#autotoc_md3',1,'']]],
+  ['wire_20library_2',['Migrating from Wire Library',['../index.html#autotoc_md12',1,'']]]
+];
