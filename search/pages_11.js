fbbb899566306b2c19d288be95ac64a86fe90b03
unknown <odoul28@gmail.com>
2026-06-26 14:38:14 -0700

    Add missing docs files

---
+++ search/pages_11.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['settings_0',['Core Settings',['../index.html#autotoc_md14',1,'']]],
+  ['start_1',['Quick Start',['../index.html#autotoc_md7',1,'']]]
+];
---
+++ search/pages_12.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['the_20library_0',['1. Include the library',['../index.html#autotoc_md8',1,'']]]
+];
