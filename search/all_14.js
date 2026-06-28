b73afe42c050571e087f598904747deaa7d64be7
unknown <odoul28@gmail.com>
2026-06-28 08:53:56 -0700

    Declare error macros in ArduboyI2C.h and fix handshaking bug

--- search/all_14.js
+++ search/all_14.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['the_20library_0',['1. Include the library',['../index.html#autotoc_md8',1,'']]],
-  ['tw_5fsuccess_1',['TW_SUCCESS',['../_arduboy_i2_c_8h.html#aaa8a379ca27da629fb7eb67cacc5d86a',1,'ArduboyI2C.h']]]
+  ['the_20library_0',['1. Include the library',['../index.html#autotoc_md8',1,'']]]
 ];
--- search/defines_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tw_5fsuccess_0',['TW_SUCCESS',['../_arduboy_i2_c_8h.html#aaa8a379ca27da629fb7eb67cacc5d86a',1,'ArduboyI2C.h']]]
-];
