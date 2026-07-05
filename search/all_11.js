2897244ebf97f7f59ef7313bf7c8ecaf6b7eeeac
unknown <odoul28@gmail.com>
2026-07-04 19:44:55 -0700

    Update docs

--- search/all_11.js
+++ search/all_11.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['none_0',['None',['../class_i2_c.html#aba8a1edcb115147e6533fc73057a4c83a6adf97f83acf6453d4a6a4b1070f3754',1,'I2C::None'],['../class_i2_c.html#ae6925af980882cb59a4fa4443d3c2e71a6adf97f83acf6453d4a6a4b1070f3754',1,'I2C::None']]],
+  ['none_0',['None',['../class_i2_c.html#aba8a1edcb115147e6533fc73057a4c83a6adf97f83acf6453d4a6a4b1070f3754',1,'I2C']]],
   ['nulladdress_1',['nullAddress',['../class_i2_c.html#ae6f7ec66d016c5daef143832e83cc252',1,'I2C']]]
 ];
